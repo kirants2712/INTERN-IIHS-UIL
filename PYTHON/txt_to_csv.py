4d95cdb56fed9c86ac68eee783a409fffdce32ca
kirants <kirants2712@gmail.com>
2019-05-22 10:29:29 +0530

    Update txt_to_csv.py

--- PYTHON/txt_to_csv.py
+++ PYTHON/txt_to_csv.py
@@ -1,7 +1,7 @@
 import csv
 import pandas as pd
 
-with open('<name of the file>.txt', 'r') as in_file:
+with open('<name of the input file>.txt', 'r') as in_file:
     stripped = (line.strip() for line in in_file)
     lines = (line.split("|") for line in stripped if line)
     with open('<name of output file>.csv', 'w') as out_file:
