7d1ad271ff43f27ab01120304b4130c3cedf3f35
Kiran T S <kiran_b160244cs@nitc.ac.in>
2019-07-05 11:24:54 +0530

    Delete txt_to_csv.py

--- PYTHON/txt_to_csv.py
+++
@@ -1,9 +0,0 @@
-import csv
-import pandas as pd
-
-with open('<name of the input file>.txt', 'r') as in_file:
-    stripped = (line.strip() for line in in_file)
-    lines = (line.split("|") for line in stripped if line)
-    with open('<name of output file>.csv', 'w') as out_file:
-        writer = csv.writer(out_file)
-        writer.writerows(lines)
