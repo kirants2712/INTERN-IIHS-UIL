7d87424083f8307dcd843824fb1ee1f2954a7c53
Kiran T S <kirants2712@gmail.com>
2020-05-16 21:19:43 +0530

    Update setup.py

--- Custom_Bangalore_Address_Parser/setup.py
+++ Custom_Bangalore_Address_Parser/setup.py
@@ -1,7 +1,7 @@
 try:
     from setuptools import setup
 except ImportError :
-    raise ImportError("setuptools module required, please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
+    raise ImportError("setuptools module required,please go to https://pypi.python.org/pypi/setuptools and follow the instructions for installing setuptools")
 
 setup(
     version='0.1',
